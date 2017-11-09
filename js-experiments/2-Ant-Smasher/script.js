98dda8526fad8d2cd9ab8ffcda04c1f5f4820f23
Basanta78 <basmhzn78@gmail.com>
2017-11-09 23:11:19 +0545

    onclick added

--- js-experiments/2-Ant-Smasher/script.js
+++ js-experiments/2-Ant-Smasher/script.js
@@ -7,8 +7,8 @@ function Ant(){
 	var that = this;
 	this.init =function(x,y){
 			this.element = document.createElement('div');
-			this.element.style.width = "50px";
-			this.element.style.height = "50px";
+			this.element.style.width = "40px";
+			this.element.style.height = "40px";
 			this.element.style.background = "blue";
 			this.element.style.position = "absolute";
 			// this.element.style.background="url('ant.png')";
@@ -20,7 +20,7 @@ function Ant(){
 	
 
 	this.move = function(){
-			if(that.left>=450)
+			if(that.left>=460)
 			{
 				that.horizontal=-1;
 			}
@@ -32,7 +32,7 @@ function Ant(){
 			{
 				that.vertical= 1;
 			}
-			else if(that.top>=450)
+			else if(that.top>=460)
 			{
 				that.vertical = -1;
 			}
@@ -67,8 +67,8 @@ function Ant(){
 		}
 
 checkCollision = function(objA,objB){
-		console.log(objA.left,objB.left);
-	if((objA.left+50>objB.left) && (objA.left<objB.left+50) && (objA.top+50>objB.top) && (objA.top<objB.top+50))
+		// console.log(objA.left,objB.left);
+	if((objA.left+40>objB.left) && (objA.left<objB.left+40) && (objA.top+40>objB.top) && (objA.top<objB.top+40))
 	{
 	return true;
 	}
@@ -77,14 +77,19 @@ checkCollision = function(objA,objB){
 		}
 	}
 
-		
+gameloop = function(){
 var objectArray = [];
 for (var i = 0;i<6;i++)
 {
 	var ant = new Ant();
+	// console.log(this);
 	objectArray.push(ant);
-	ant.init(Math.floor(Math.random()*500+1),Math.floor(Math.random()*500+1));
-	ant.onclick = function(){
+	ant.init(Math.floor(Math.random()*400+1),Math.floor(Math.random()*400+1));
+	console.log(objectArray);
+	ant.element.onclick = function(){
+		console.log(this);
+		this.style.left=0;
+		this.style.top=0;
 		main.removeChild(this);
 	}
 	
@@ -106,7 +111,27 @@ setInterval(function(){
 
 	}
 
-},10);	
+},10);
+}	
+var startButton = document.createElement('button');
+document.getElementsByTagName('body')[0].appendChild(startButton);
+startButton.innerText = 'Start Game';
+
+startButton.onclick = function(){
+    startButton.style.display = 'none';
+    gameOver.style.display = 'none';
+    gameloop();
+
+}
+var gameOver = document.createElement('div');
+main.appendChild(gameOver);
+gameOver.style.fontSize = '50px';
+gameOver.style.color = 'red';
+gameOver.style.position = 'absolute';
+gameOver.style.display = 'none';
+gameOver.style.top = '20px';
+gameOver.style.left = '230px';
+gameOver.innerHTML = 'GAME OVER !!';
 
 
 
